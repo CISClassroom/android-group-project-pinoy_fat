9fbcf3dabfae4ab9b03791e19b612b8cbf5722c2
CHEATDATA <31352194+aonza412@users.noreply.github.com>
2020-03-22 01:02:43 +0700

    sub

--- app/src/main/java/th/ac/kku/cis/mobileapp/myapplication2/MainActivity.kt
+++ app/src/main/java/th/ac/kku/cis/mobileapp/myapplication2/MainActivity.kt
@@ -10,9 +10,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var mDatabase: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         mDatabase = FirebaseDatabase.getInstance().reference
 
         button2.setOnClickListener {
